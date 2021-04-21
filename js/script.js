ed70f9049f55c96c4bd65ffabe14f244c3212344
Natalya <example@gmail.com>
2021-04-21 11:35:10 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,51 @@
+"use strict";
+
+
+let number = 5;
+const leftBorderWidth = 1;
+
+number = 10;
+console.log(number);
+
+// const obj = {
+//     a:50
+// };
+// obj.a = 10;
+// console.log(obj);
+
+const obj = {
+    name: "Lory",
+    age: 25,
+    isMarried: false
+};
+// console.log(obj.name);
+console.log(obj['name']);
+
+// alert('Hello!');
+
+// const result = confirm('Are you here');
+// console.log(result);
+
+// const answer = prompt('Do you old 18?', '18');
+// console.log(answer + 5);
+
+// const answer = +prompt('Do you old 18?', '18');
+// console.log(answer + 5);
+
+// const answers = [];
+
+// answers[0] = prompt('Do you have name?', '');
+// answers[1] = prompt('Do you have family?', '');
+// answers[2] = prompt('How old are you?', '');
+
+// document.write(answers);
+// console.log(typeof(answers));
+
+// интерполяция
+
+const category = 'toys';
+
+console.log(`https://someurl.com/${category}/2`);
+
+const user = "Peter";
+alert(`Privet, ${user}`);
